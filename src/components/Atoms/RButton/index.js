25f64ab192097a5947dd58365da15160921c0959
Rafael Rocha <rafael.rocha@mindera.com>
2018-09-12 15:00:58 +0100

    RButton finalized for styleguide testing

--- src/components/Atoms/RButton/index.js
+++ src/components/Atoms/RButton/index.js
@@ -7,18 +7,31 @@ import styles from './index.module.scss';
 class RButton extends PureComponent {
   render() {
     const { text, onClick, danger, primary, secondary } = this.props;
-    const buttonClass = cx(styles.button, { ['--primary']: primary });
+    const buttonClass = cx(
+      styles.button,
+      { ['--primary']: primary },
+      { ['--danger']: danger },
+      { ['--secondary']: secondary }
+    );
 
-    return <button className={buttonClass}>{text}</button>;
+    return <button className={buttonClass} onClick={onClick}>{text}</button>;
   }
 }
 
 RButton.defaultProps = {
   text: '',
+  primary: true,
+  secondary: false,
+  danger: false,
+  onClick: () => {},
 };
 
 RButton.propTypes = {
   text: PropTypes.string,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  danger: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 RButton.displayName = 'RButton';
