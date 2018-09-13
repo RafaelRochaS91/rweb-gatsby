a63be4f2117fc9509b65c87828914c913d8ad9d0
Rafael Rocha <rafael.rocha@mindera.com>
2018-09-14 00:10:55 +0100

    Added mixins and some more stylguide components

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,8 +5,7 @@ exports.components = {
   "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/404.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/index.js"),
-  "component---src-pages-styleguide-index-js": require("gatsby-module-loader?name=component---src-pages-styleguide-index-js!/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/styleguide/index.js"),
-  "component---src-pages-styleguide-components-buttons-list-js": require("gatsby-module-loader?name=component---src-pages-styleguide-components-buttons-list-js!/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/styleguide/components/ButtonsList.js")
+  "component---src-pages-styleguide-index-js": require("gatsby-module-loader?name=component---src-pages-styleguide-index-js!/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/styleguide/index.js")
 }
 
 exports.json = {
@@ -14,9 +13,8 @@ exports.json = {
   "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/dev-404-page.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/404.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/index.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/404-html.json"),
   "styleguide.json": require("gatsby-module-loader?name=path---styleguide!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/styleguide.json"),
-  "styleguide-components-buttons-list.json": require("gatsby-module-loader?name=path---styleguide-components-buttons-list!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/styleguide-components-buttons-list.json")
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/404-html.json")
 }
 
 exports.layouts = {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -10,8 +10,7 @@ exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/index.js")),
-  "component---src-pages-styleguide-index-js": preferDefault(require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/styleguide/index.js")),
-  "component---src-pages-styleguide-components-buttons-list-js": preferDefault(require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/styleguide/components/ButtonsList.js"))
+  "component---src-pages-styleguide-index-js": preferDefault(require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/src/pages/styleguide/index.js"))
 }
 
 exports.json = {
@@ -19,7 +18,6 @@ exports.json = {
   "dev-404-page.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/dev-404-page.json"),
   "404.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/404.json"),
   "index.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/index.json"),
-  "404-html.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/404-html.json"),
   "styleguide.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/styleguide.json"),
-  "styleguide-components-buttons-list.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/styleguide-components-buttons-list.json")
+  "404-html.json": require("/Users/rafarocha91/Desktop/projects/rweb-gatsby/.cache/json/404-html.json")
 }
